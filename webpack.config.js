3054a9bde11e3a249a3d59179201e843e12365e9
cristiano salvatore anselmi <cristiano.anselmi@gmail.com>
2020-06-04 01:37:06 +0200

    chore: add project configuration and sample component

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const path = require('path');
+
+module.exports = {
+	mode: "production",
+	devtool: "source-map",
+	resolve: {
+		extensions: [".ts", ".tsx"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "ts-loader"
+					}
+				]
+			},
+			{
+				enforce: "pre",
+				test: /\.js$/,
+				loader: "source-map-loader"
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+		   template: path.resolve( __dirname, 'public/index.html' ),
+		   filename: 'index.html'
+		})
+	],
+	externals: {
+		"react": "React",
+		"react-dom": "ReactDOM"
+	},
+	devServer: {
+		compress: true,
+		historyApiFallback: true,
+		host: process.env.HOST || 'localhost',
+		contentBase: path.resolve( __dirname),
+		watchOptions: {
+			ignored: /node_modules/
+		},
+		quiet: true,
+		hot: true,
+		http2: true
+	},
+};
