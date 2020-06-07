7eabe81ae685ee34581c4897b5cd4af756eebe4a
cristiano salvatore anselmi <cristiano.anselmi@gmail.com>
2020-06-07 14:44:26 +0200

    chore: split chunks instead of externals
    add tenser plugin for better bundle optimization
    add base tag element
    move index under public folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,45 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
 	mode: "production",
 	devtool: "source-map",
 	resolve: {
 		extensions: [".ts", ".tsx", ".js", ".css", ".svg"]
+	},
+	entry: {
+		main: "./src/index.tsx",
+		vendors: [
+			"axios",
+			"react",
+			"react-dom",
+			"react-styleable",
+			"react-test-renderer"
+		]
+	},
+	output: {
+		path: path.join(__dirname, './dist'),
+		filename: '[name].js',
+		publicPath: '/',
+	},
+	optimization: {
+		usedExports: true,
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({cache: true,  parallel: 4, extractComments: false})
+		],
+		splitChunks: {
+		  cacheGroups: {
+			vendor: {
+			  chunks: 'initial',
+			  name: 'vendor',
+			  test: '/node_modules/',
+			  enforce: true
+			},
+		  }
+		},
+		runtimeChunk: false
 	},
 	module: {
 		rules: [
@@ -47,19 +81,15 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebPackPlugin({
-		   template: path.resolve( __dirname, 'index.html' ),
-		   filename: 'index.html'
+			template: path.resolve( __dirname, 'public/index.html' ),
+			filename: 'index.html'
 		})
 	],
-	externals: {
-		"react": "React",
-		"react-dom": "ReactDOM"
-	},
 	devServer: {
 		compress: true,
 		historyApiFallback: true,
 		host: process.env.HOST || 'localhost',
-		contentBase: path.resolve( __dirname),
+		contentBase: path.join( __dirname, 'dist'),
 		watchOptions: {
 			ignored: /node_modules/
 		},
