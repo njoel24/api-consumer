1e400ff35333bb8b38f6b06f29ca1139a8c178ac
cristiano salvatore anselmi <cristiano.anselmi@gmail.com>
2020-06-11 02:02:15 +0200

    Revert "test: verify axios call being called within useEffect"
    This reverts commit 0f0bc1d8b91864ab391a25d3655f9033b5465c34.

--- src/components/container/container.spec.tsx
+++ src/components/container/container.spec.tsx
@@ -1,31 +1,11 @@
 import React from 'react';
-import renderer, { ReactTestRenderer } from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import Container from './container.component';
 
-const mockData = [{state: "Basel-Landschaft"}];
-const myAxios = () => Promise.resolve(mockData);
-jest.mock('axios', myAxios);
-
 describe('Container', () => {
-	let component: ReactTestRenderer;
-
-	beforeEach(() => {
-		component = renderer.create(<Container/>);
-	});
-
-	afterEach(() => {
-		jest.resetAllMocks();
-		jest.restoreAllMocks();
-	});
-
 	it('renders component and compare with snapshot', () => {
+		const component = renderer.create(<Container/>);
 		const tree = component.toJSON();
 		expect(tree).toMatchSnapshot();
-	});
-
-	it('should run an effect right after rendering and invoke axios within that effect', async () => {
-		setTimeout(() => {
-			expect(JSON.stringify(component)).toMatch("Basel-Landschaft");
-		}, 5000);
 	});
 });
